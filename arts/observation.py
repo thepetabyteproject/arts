08e0b2b206355c3f2cc1a4c52388730796bd7dfa
Evan Lewis <elewis@bowser.phys.wvu.edu>
2022-02-17 12:26:47 -0500

    Create DM delay code and tests.

--- arts/observation.py
+++ arts/observation.py
@@ -34,12 +34,26 @@ class Observation(Telescope):
         """
 
         Args:
-            dm:
+            dm (float): dispersion measure (pc/cm^3)
 
-        Returns:
+        Returns: 
+            t_delay_bw (float): dispersion delay across bandwidth in ms
+            t_delay_lowchan (float): dispersion delay across lowest frequency channel in ms
+            t_delay_hichan (float): dispersion delay across highest frequency channel in ms
 
         """
-        return 0
+        hf = self.center_freq + (self.bandwidth/2.)
+        lf = self.center_freq - (self.bandwidth/2.)
+        #Make sure frequencies are in MHz otherwise this is wrong
+
+        chan_width = self.bandwidth/self.nchans
+        lowchan_end = lf + chan_width #upper frequency of lowest-frequency channel
+        hichan_start = hf - chan_width #lower frequency of highest-frequency channel
+
+        t_delay_bw = 4148808 * dm * ( (lf**(-2)) - (hf**(-2)) ) #delay across BW in ms
+        t_delay_lowchan = 4148808 * dm * ( (lf**(-2)) - (lowchan_end**(-2)) ) #delay across lowest channel in ms
+        t_delay_hichan = 4148808 * dm * ( (hichan_start**(-2)) - (hf**(-2)) ) #delay across highest channel in ms
+        return t_delay_bw, t_delay_lowchan, t_delay_hichan
 
     def dm_smear_intrachan(self):
         """
--- tests/test_observation.py
+++ tests/test_observation.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pytest
 from arts.observation import Observation
 from arts.telescope import Telescope
 
@@ -17,4 +17,21 @@ def test_min_flux():
 
 
 def test_dm_delay():
-    assert 1 == 1
+    myobs = Observation()
+    setattr(myobs,'bandwidth',100)
+    setattr(myobs,'center_freq',350)
+    setattr(myobs,'nchans',512)
+
+    #Check that delay = 0 when DM = 0
+    for i in range(3):
+        assert pytest.approx(obs.dm_delay(dm=0)[i], rel=1e-6) == 0.
+
+    #Check math for known calculation
+    assert pytest.approx(obs.dm_delay(dm=20)[0], rel=1e-3) == 403.356333333334
+    assert pytest.approx(obs.dm_delay(dm=20)[1], rel=1e-3) == 1.19929396522448
+    assert pytest.approx(obs.dm_delay(dm=20)[2], rel=1e-3) == 0.50681746304299
+
+    #Check that delay = 0 when bandwidth = 0
+    setattr(myobs,'bandwidth',0)
+    for i in range(3):
+       assert pytest.approx(myobs.dm_delay(50)[i],rel=1e-6) == 0.
