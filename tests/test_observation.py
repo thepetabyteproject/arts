52c1085acfe0aafdd8e43b9cfb525a6064c57ae3
GrahamDoskoch <grahamdoskoch@gmail.com>
2022-02-25 16:55:24 -0500

    Added tests for DM to distance conversion.

--- tests/test_observation.py
+++ tests/test_observation.py
@@ -1,4 +1,8 @@
 import numpy as np
+import pygedm
+import pytest
+from astropy import units as u
+from astropy.coordinates import SkyCoord
 
 from arts.observation import Observation
 from arts.telescope import Telescope
@@ -18,3 +22,13 @@ def test_min_flux():
 
 def test_dm_delay():
     assert 1 == 1
+
+    
+def test_dm_to_dist():
+    obs = Observation()
+    
+    assert obs.dm_to_dist('00h00m00s', '00d00m00s', 0, 'ne2001') == 0
+    assert obs.dm_to_dist('00h00m00s', '00d00m00s', 0, 'ymw16') == 0
+    
+    assert pytest.approx(obs.dm_to_dist('17h45m40s', '-29d00m28s', 10, 'ne2001'), rel=1e-6) == '422.4824905395508 pc'
+    assert pytest.approx(obs.dm_to_dist('17h45m40s', '-29d00m28s', 10, 'ymw16'), rel=1e-6) == '517.3521728515625 pc'
