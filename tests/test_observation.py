75e6d3a1c661f4443031aa087bf36a416cff0e77
GrahamDoskoch <grahamdoskoch@gmail.com>
2022-03-11 20:00:22 -0500

    Fixed test formatting and missing .value.

--- tests/test_observation.py
+++ tests/test_observation.py
@@ -51,11 +51,9 @@ def test_dm_to_dist():
     assert obs.dm_to_dist("00h00m00s", "00d00m00s", 0, "ne2001").value == 0
     assert obs.dm_to_dist("00h00m00s", "00d00m00s", 0, "ymw16").value == 0
 
-    assert (
-        pytest.approx(obs.dm_to_dist("17h45m40s", "-29d00m28s", 10, "ne2001").value, rel=1e-6)
+    assert pytest.approx(obs.dm_to_dist("17h45m40s", "-29d00m28s", 10, "ne2001").value, rel=1e-6)
         == 422.4824905395508
     )
-    assert (
-        pytest.approx(obs.dm_to_dist("17h45m40s", "-29d00m28s", 10, "ymw16").value, rel=1e-6)
+    assert pytest.approx(obs.dm_to_dist("17h45m40s", "-29d00m28s", 10, "ymw16").value, rel=1e-6)
         == 517.3521728515625
     )
